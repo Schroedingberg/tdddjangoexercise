a1b57ecc3b0e80ebe85e311ab048e2ce790903a8
Aaron Meinel <aaronrebmann@gmail.com>
2021-04-19 18:45:01 +0200

    Extend FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,6 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_layout_and_styling(self):
         #Edith goes to the home page
@@ -32,19 +44,19 @@ class NewVisitorTest(LiveServerTestCase):
             delta = 10
         )
 
+        #She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta = 10
+
+        )
         
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
