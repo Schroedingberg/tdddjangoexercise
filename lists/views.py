c3c27c668bccff0b3ace2ceb6e62aff30217a358
Aaron Meinel <aaronrebmann@gmail.com>
2021-04-23 23:33:29 +0200

    Rename ids, write helper function for getting items, use new form in home page

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from lists.models import Item, List
+from lists.forms import ItemForm
 from django.core.exceptions import ValidationError
 from django.utils.html import escape
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
@@ -14,7 +15,7 @@ def view_list(request, list_id):
     
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -26,7 +27,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text = request.POST['item_text'], list=list_)
+    item = Item.objects.create(text = request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
